1632738fdb25c12cf801c25a098fde3b683673a6
= <=>
2025-09-17 20:27:51 -0400

    Completed Lesson 2.4 - Collision Decisions
    Created custom function for spawning random animals on a timed interval.
    
    Added collider and trigger components to animal prefabs for proper object collision.
    
    Created a script using OnTriggerEvent to destroy animal objects upon their coming into contact with food.
    
    Added box colliders as triggers to the animal and food prefabs and added rigidbody component to the food prefab.
    
    Created DetectCollisions script to destroy animal and food game objects that have collided.

--- Units/Basic Gameplay/Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Units/Basic Gameplay/Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -8,6 +8,8 @@ public class SpawnManager : MonoBehaviour
     // values to aide in random animal generation location
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
 
     // index to aide random animal generation
     // public int animalIndex;
@@ -15,17 +17,22 @@ public class SpawnManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // Spawn animals based on an interval
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S)) {
-            // Randomly generate animal index and spawn position
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-        }
+        // if (Input.GetKeyDown(KeyCode.S)) {
+        //   SpawnRandomAnimal();
+        // }
+    }
+
+    void SpawnRandomAnimal() {
+          // Randomly generate animal index and spawn position
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
