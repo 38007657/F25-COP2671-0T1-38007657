9c020c77b15b7044553a804f1ade4cd30691aed5
Kelsey Hodge <38007657@sfcollege.edu>
2025-10-08 11:30:00 -0400

    Completed Lesson 5.3 - Game Over
    Added a TextMeshPro object to the canvas and modified its attributes for placement and style.
    
    Modified the GameManager and Target scripts and added a tag to the bad object to show the game over text only if a good object falls out of the scene.
    
    Modified both scripts by adding a condition for checking that the game is active, and if the game is not active stops objects from spawning.
    
    Added a Restart UI button to allow the player to play the game again after losing.
    
    Added Restart button and modified its font, size, and color.
    
    Modified the GameManager script by adding a RestartGame method and attaching it to our Restart button and set it to restart the game when clicked.
    
    Modified GameManager script to only show the Restart button when the game is not active.

--- Units/User Interface/Prototype 5/Assets/Scripts/GameManager.cs
+++ Units/User Interface/Prototype 5/Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class GameManager : MonoBehaviour
@@ -10,14 +12,17 @@ public class GameManager : MonoBehaviour
     private float spawnRate = 1.0f;
     public TextMeshProUGUI scoreText;
     private int score;
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
+    public Button restartButton;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        isGameActive = true;
         StartCoroutine(SpawnTarget());
         score = 0;
         UpdateScore(0);
-        
     }
 
     // Update is called once per frame
@@ -28,7 +33,7 @@ public class GameManager : MonoBehaviour
 
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
@@ -40,5 +45,17 @@ public class GameManager : MonoBehaviour
     {
         score += scoreToAdd;
         scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
--- Units/User Interface/Prototype 5/Assets/Scripts/Target.cs
+++ Units/User Interface/Prototype 5/Assets/Scripts/Target.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
 
 public class Target : MonoBehaviour
 {
@@ -45,13 +47,22 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown()
     {
-        Destroy(gameObject);
-        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
-        gameManager.UpdateScore(pointValue);
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
+        
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
+        
     }
 }
